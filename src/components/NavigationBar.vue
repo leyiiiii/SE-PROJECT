5e230056b1c6fe61ae07a98ebe2bbd1ffa5555e0
PikaTea <pqy138@gmail.com>
2022-08-01 14:38:40 +0800

    Nav

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="nav">
+        <p>Nav</p>
+    </div>
+</template>
+
+<style scoped>
+.nav{
+    background-color: red;
+    height: 500px;
+    width: 50px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Navi></Navi>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Navi from '@/components/NavigationBar.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    Navi,
   }
 }
 </script>

6a3d064e3c3e63d1778e39e78993bf2aeea21d3b
PikaTea <pqy138@gmail.com>
2022-08-02 07:27:36 +0800

    Team

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -28,6 +28,40 @@
                 <span class="membersTitle">成员列表</span>
                 </div></el-col>
         </el-row>
+        <el-row class="membersRow">
+        <el-row v-for="item in membersList" :key="item.id" justify="center" type="flex">
+            <el-col :span="23"><div class="membersList">
+            <el-row>
+                <el-col :span="10">
+                    <div class="membersInfo">
+                        <span>昵称:&nbsp;</span>
+                        <span>{{ item.username }}</span>
+                </div></el-col>
+                <el-col :span="10">
+                    <div class="membersInfo">
+                        <span>真实姓名:&nbsp;</span>
+                        <span>{{ item.realname }}</span>
+                </div></el-col>
+                <el-col :span="4"><div class="buttonArea">
+                <el-button class="promoteButton" type="success" round>升为管理员</el-button>
+                <el-button class="kickButton" type="danger" round>移除</el-button>
+                </div></el-col>
+            </el-row>
+            <el-row>
+                <el-col :span="10">
+                    <div class="membersInfo">
+                        <span>邮箱:&nbsp;</span>
+                        <span>{{ item.email }}</span>
+                </div></el-col>
+                <el-col :span="10">
+                    <div class="membersInfo">
+                        <span>身份:&nbsp;</span>
+                        <span>{{ item.pos }}</span>
+                </div></el-col>
+            </el-row>
+            </div></el-col>
+        </el-row>
+        </el-row>
         </div></el-col>
     </el-row>
 </template>
@@ -39,6 +73,67 @@ export default {
   name: 'HomeView',
   components: {
     Navi,
+  },
+  data() {
+    return {
+        membersList:[{
+            id:1,
+            username:'Lele',
+            realname:'HuangLele',
+            pos:'Owner',
+            email:'abc@gmail.com',
+        },
+        {
+            id:2,
+            username:'Lolo',
+            realname:'HuangLolo',
+            pos:'Admin',
+            email:'efg@gmail.com',
+        },
+        {
+            id:3,
+            username:'Lili',
+            realname:'HuangLili',
+            pos:'Member',
+            email:'hij@gmail.com',
+        },
+                {
+            id:4,
+            username:'Lili',
+            realname:'HuangLili',
+            pos:'Member',
+            email:'hij@gmail.com',
+        },
+        {
+            id:5,
+            username:'Lili',
+            realname:'HuangLili',
+            pos:'Member',
+            email:'hij@gmail.com',
+        },
+        {
+            id:6,
+            username:'Lili',
+            realname:'HuangLili',
+            pos:'Member',
+            email:'hij@gmail.com',
+        },
+        {
+            id:7,
+            username:'Lili',
+            realname:'HuangLili',
+            pos:'Member',
+            email:'hij@gmail.com',
+        },
+        {
+            id:8,
+            username:'Lili',
+            realname:'HuangLili',
+            pos:'Member',
+            email:'hij@gmail.com',
+        },
+        ]
+    }
   }
 }
 </script>
@@ -51,7 +146,7 @@ export default {
     margin: 10px;
 }
 .teamInfo1{
-    font-size: 36px;
+    font-size: 40px;
 }
 .teamInfo2{
     font-size: 20px;
@@ -62,6 +157,38 @@ export default {
 .membersTitle{
     font-size: 24px;
 }
+.membersList{
+    /* border: 1px solid black; */
+    border-radius: 20px;
+    margin-top: 10px;
+    background-color: orange;
+}
+.membersInfo{
+    margin: 5px 0 5px 10px;
+    font-size: 18px;
+    /* border: 1px solid black; */
+}
+.membersRow{
+    /* border: 1px solid black; */
+    height: 550px;
+    overflow: hidden;
+    overflow-y: scroll;
+}
+.buttonArea{
+    position: absolute;
+    margin-top: 12px;
+}
+/* .kickButton{
+    position: absolute;
+    margin: 0 0 0 130px;
+    top: 40%;
+    float: right;
+} */
+/* .promoteButton{
+    position: absolute;
+    margin: 0;
+    top: 40%;
+} */
 .el-icon-user-solid{
     font-size: 22px;
 }

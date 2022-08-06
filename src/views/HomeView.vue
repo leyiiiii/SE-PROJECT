07858faa5ed8a3f44536fc73dc113fdac29f9149
70216002黄乐怡 <mikiwongly@hotmail.my>
2022-08-06 13:49:58 +0800

    homepage ui

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="home">
-    <el-row>
-      <el-col :span="2"><div class="Left">
-        <Navi></Navi>
-        </div></el-col>
-      <el-col :span="22"><div class="Right">
-        <img id="bg" src="@/assets/Workhard3.jpg"/>
-        <el-row justify="center" type="flex">
+    <Header></Header>
+    <div>
+      <img id="bg" src="@/assets/HomepageBg.png" />
+      <h1 class="slogan">
+        <span class="text-wrapper">
+          <span class="line line1"></span>
+          <span class="letters">产品协作设计，更快更简单！</span>
+        </span>
+      </h1>
+      <h4 class="slogan2">
+        <span>设计师的利器，产品经理的帮手，工程师的伙伴</span>
+      </h4>
+      <!-- <el-row justify="center" type="flex">
           <el-col :span="12"><div class="bigIcon">
             <i class="el-icon-office-building"></i>
           </div></el-col>
-        </el-row>
-        <el-row>
+        </el-row> -->
+      <!-- <el-row>
           <el-col :span="24"><div class="Title">
             <span>没有团队？创一个？加入一个？</span>  
           </div></el-col>
@@ -37,66 +43,108 @@
               <el-button type="primary" icon="el-icon-message" @click="checkInvitation">查看邀请</el-button>
             </el-button-group>
           </div></el-col>
-        </el-row>
-        <el-row v-else type="flex" align="middle">
+        </el-row> -->
+      <!-- <el-row v-else type="flex" align="middle">
           <el-col :span="24"><div class="goLogin">
             <span>马上登录吧</span>
             <i class="el-icon-right"></i>
             <el-button type="primary" @click="toLogin">登录</el-button>
           </div></el-col>
-        </el-row>
-      </div></el-col>
-    </el-row>
+        </el-row> -->
+    </div>
   </div>
 </template>
-
 <script>
-import Navi from '@/components/NavigationBar.vue'
+import Header from "@/components/Header.vue";
 import user from "@/store/user";
+import anime from "animejs/lib/anime.js";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    Navi,
+    Header,
   },
   data() {
     return {
       dialogVisible: false,
       team: {
-        name:'',
-        desc:'',
+        name: "",
+        desc: "",
       },
       isLogin: false,
-    }
+    };
   },
   created() {
     var userInfo;
     userInfo = user.getters.getUser(user.state());
-    if(userInfo) {
+    if (userInfo) {
       this.userId = userInfo.user.id;
       this.isLogin = true;
     }
   },
+  mounted() {
+    var textWrapper = document.querySelector(".slogan .letters");
+    textWrapper.innerHTML = textWrapper.textContent.replace(
+      /([^\x00-\x80]|\w)/g,
+      "<span class='letter'>$&</span>"
+    );
+
+    anime
+      .timeline({ loop: false })
+      .add({
+        targets: ".slogan .line",
+        scaleY: [0, 1],
+        opacity: [0.5, 1],
+        easing: "easeOutExpo",
+        duration: 700,
+      })
+      .add({
+        targets: ".slogan .line",
+        translateX: [
+          0,
+          document.querySelector(".slogan .letters").getBoundingClientRect()
+            .width + 10,
+        ],
+        easing: "easeOutExpo",
+        duration: 700,
+        delay: 100,
+      })
+      .add({
+        targets: ".slogan .letter",
+        opacity: [0, 1],
+        easing: "easeOutExpo",
+        duration: 600,
+        offset: "-=775",
+        delay: (el, i) => 34 * (i + 1),
+      });
+    anime.timeline({ loop: false }).add({
+      targets: ".slogan2",
+      opacity: [0, 1],
+      easing: "easeOutExpo",
+      duration: 3000,
+      delay: 3000,
+    });
+  },
   methods: {
     cancelChanges() {
       this.dialogVisible = false;
-      this.team.name = '';
-      this.team.desc = '';
+      this.team.name = "";
+      this.team.desc = "";
     },
     handleClose() {
       this.dialogVisible = false;
-      this.team.name = '';
-      this.team.desc = '';
+      this.team.name = "";
+      this.team.desc = "";
     },
     createTeam() {
-      if(this.team.name == "") {
+      if (this.team.name == "") {
         this.$message.warning("团队名不可为空");
         return;
       }
-            
-      if(this.team.desc == "") {
-          this.$message.warning("团队简介不可为空");
-          return;
+
+      if (this.team.desc == "") {
+        this.$message.warning("团队简介不可为空");
+        return;
       }
 
       const formData = new FormData();
@@ -108,58 +156,95 @@ export default {
         header = { Authorization: "Bearer " + localStorage.getItem("token") };
 
       this.$axios({
-        method:"post",
-        url:"/api/v1/team/create",
+        method: "post",
+        url: "/api/v1/team/create",
         data: formData,
         headers: header,
       })
-        .then((res) =>{
+        .then((res) => {
           console.log(res);
           this.$message.success("创建成功");
           this.dialogVisible = false;
-          this.team.name = '';
-          this.team.desc = '';
+          this.team.name = "";
+          this.team.desc = "";
         })
-        .catch((err) =>{
+        .catch((err) => {
           console.log(err);
-        })
+        });
     },
     checkInvitation() {
       this.$router.push("/invitation");
     },
     toLogin() {
       this.$router.push("/login");
-    }
-  }
-}
+    },
+  },
+};
 </script>
 
 <style scoped>
-.Title{
+.slogan2 {
+  margin: 0 120px;
+  color: #3D4777;
+}
+.slogan {
+  font-weight: 700;
+  font-size: 3.5em;
+  margin: 200px 120px 20px;
+}
+
+.slogan .text-wrapper {
+  position: relative;
+  display: inline-block;
+  padding-top: 0.1em;
+  padding-right: 0.05em;
+  padding-bottom: 0.15em;
+}
+
+.slogan .line {
+  opacity: 0;
+  position: absolute;
+  left: 0;
+  height: 100%;
+  width: 3px;
+  background-color: #fff;
+  transform-origin: 0 50%;
+}
+
+.slogan .line1 {
+  top: 0;
+  left: 0;
+}
+
+.slogan .letter {
+  display: inline-block;
+  line-height: 1em;
+}
+.Title {
   text-align: center;
   font-size: 64px;
   font-family: STKaiti;
 }
-.bigIcon{
+.bigIcon {
   /* border: 1px solid black; */
   text-align: center;
   margin-top: 180px;
   font-size: 100px;
 }
-.Button{
+.Button {
   text-align: center;
   margin-top: 20px;
 }
-.Button button{
+.Button button {
   background-color: rgba(154, 85, 85, 0.664);
   border-color: rgb(140, 83, 83);
 }
-.goLogin{
+.goLogin {
   /* border: 1px solid black; */
   text-align: center;
   font-size: 36px;
 }
-.goLogin button{
+.goLogin button {
   width: 120px;
   font-size: 30px;
   margin-top: 10px;
@@ -169,11 +254,15 @@ export default {
 #bg {
   width: 100%;
   position: absolute;
+  top: 0;
   z-index: -1;
-  object-fit: cover;
-  opacity: 0.8;
+  /* object-fit: cover; */
+  /* opacity: 0.8; */
 }
-#cancel{
+#cancel {
   color: white;
+}
+.home {
+  height: 100vh;
 }
 </style>
